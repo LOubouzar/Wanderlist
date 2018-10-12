084885816b59c78a7fa4e6f7449bbf74ca2125c7
Poltergeist <brandonssc@gmail.com>
2018-10-12 14:10:55 -0400

    heroku deploy

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 //change line 8 to /../config/config.js file for heroku deploy testing
 //change line 8 to /../config/config.json file for local testing
 

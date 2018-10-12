ee1d182a8416508c0b99d78f5d976cfae8700de7
Lauren <lmcdono17@gmail.com>
2018-10-12 10:14:21 -0400

    list page

--- public/js/list.js
+++ public/js/list.js
@@ -1,1 +1,15 @@
 
+//Function to add and take away items in checklist
+$(function () {
+    $("#btnAdd").bind("click", function () {
+        var div = $("<tr />");
+        div.html(GetDynamicTextBox(""));
+        $("#TextBoxContainer").append(div);
+    });
+    $("body").on("click", ".remove", function () {
+        $(this).closest("tr").remove();
+    });
+});
+function GetDynamicTextBox(value) {
+    return '<td><input name = "DynamicTextBox" type="text" value = "' + value + '" class="form-control" /></td>' + '<td><select name="" class="form-control"><option> Select</option><option>0</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select></td>' + value + '" /></td>' + '<td><input name = "DynamicTextBox" type="checkbox" value = "' + value + '" /></td>' + '<td><button type="button" class="btn btn-danger remove"><i class="fa fa-trash" aria-hidden="true"></i></button></td>'
+}

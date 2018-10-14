46d89a81d15ad475c95f417f881c530860137197
Poltergeist <brandonssc@gmail.com>
2018-10-14 19:30:50 -0400

    associate user table with many list table records

--- models/user.js
+++ models/user.js
@@ -15,6 +15,15 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     }
   });
+
+  User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.List, {
+      onDelete: "cascade"
+    });
+  };
+
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
